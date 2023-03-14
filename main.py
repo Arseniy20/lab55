98acea240ffa40943ee4ad49b13c9cde102e074d
annoxias <1369502@gmail.com>
2023-03-14 18:54:04 +0300

    ghgh

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import random import randit
 n = 2
 
 kusko = []
@@ -29,39 +28,4 @@ def lab32():
 
 lab32()
 
-
-def lab33():
-    on = 0
-    op = 0
-    while on < 3:
-        n1 = randint(1, 100)
-        n2 = randint(1, 100)
-        print(f"{n1} + {n2} = ", end="")
-        otv = input()
-        if not otv.isdigit() and otv != "stop":
-            print("Некорректный ввод, повторите попытку!")
-            otv = input()
-            if (n1 + n2) != int(otv):
-                print("Ответ неверный:(")
-                on += 1
-            if (n1 + n2) == int(otv):
-                print("Правильно!")
-                op += 1
-            if on >= 3:
-                print("Игра окончена. Правильных ответов: ", op)
-        elif otv == "stop":
-            print("Игра завершена.")
-        else:
-            if (n1 + n2) != int(otv):
-                print("Ответ неверный:(")
-                on += 1
-            if (n1 + n2) == int(otv):
-                print("Правильно!")
-                op += 1
-            if on >= 3:
-                print("Игра окончена. Правильных ответов: ", op)
-
-
-
-lab33()
 
